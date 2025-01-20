1f441891688470b5decf847f9bcb5c8baa0e319b
saxiybiz <saxiy.biz@gmail.com>
2025-01-20 14:03:31 +0500

    release 1.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="colory-pprint",
-    version="1.2.0",
+    version="1.3.0",
     description="A color-coded JSON pretty printer for Python.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
@@ -11,6 +11,7 @@ setup(
     url="https://github.com/Ruhiddin/colory-pprint",
     license="MIT",
     packages=find_packages(),
+    py_modules=['colory_pprint'],
     install_requires=[
         "termcolor",
     ],
