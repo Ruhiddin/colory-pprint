7d7a53e9581f7c8be1afce9c60e01b902800af90
saxiybiz <saxiy.biz@gmail.com>
2025-01-20 13:31:11 +0500

    refactor: version edit after refactor at project structure

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="colory-pprint",
-    version="1.0.0",
+    version="1.1.0",
     description="A color-coded JSON pretty printer for Python.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
